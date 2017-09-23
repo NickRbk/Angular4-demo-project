aa14d0defb8166659e5488abfad17c095367f19b
NickRbk <nrybak94@gmail.com>
2017-09-22 19:41:38 -0700

    setup authentication, 2 stage

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,17 +4,24 @@ import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService) {}
+  constructor(private http: Http,
+              private recipeService: RecipeService,
+              private authService: AuthService) {}
 
   storeRecipes() {
-    return this.http.put('https://angular4-demo-project.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+
+    return this.http.put(`https://angular4-demo-project.firebaseio.com/recipes.json?auth=${token}`, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    return this.http.get('https://angular4-demo-project.firebaseio.com/recipes.json')
+    const token = this.authService.getToken();
+
+    return this.http.get(`https://angular4-demo-project.firebaseio.com/recipes.json?auth=${token}`)
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
