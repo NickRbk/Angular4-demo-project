36d0e60b9245be5ef4273b32f0c07dcbccce9214
NickRbk <nrybak94@gmail.com>
2017-10-15 10:26:22 -0700

    add core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import {HighlightDirective} from './directives/highlight.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
@@ -13,20 +11,18 @@ import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HighlightDirective,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,  // contain CommonModule + Additional modules
     AppRoutingModule,
     HttpModule,
     AuthModule,
+    CoreModule,
     SharedModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
