d33bc7f1d9c9934d88b466ac5cfdd5f9ab2c1a0e
NickRbk <nrybak94@gmail.com>
2017-10-15 11:33:53 -0700

    add IoT and preloadingStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
 import { HttpModule } from '@angular/http';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
@@ -25,7 +20,6 @@ import { CoreModule } from './core/core.module';
     CoreModule,
     SharedModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
